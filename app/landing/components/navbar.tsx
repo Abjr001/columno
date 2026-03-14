75c4a15a773c9606078da653cba32bd39bab6e96
Kekeli <keliabotsidia@gmail.com>
2026-03-14 21:38:46 +0000

    responsive updated

--- app/landing/components/navbar.tsx
+++ app/landing/components/navbar.tsx
@@ -8,19 +8,18 @@ function Navbar() {
   return (
     <nav>
       <ul className="flex justify-between items-center text-white">
-        <li className=" w-1/12 flex justify-start items-center">
+        <li className="">
           <Image height={200} width={150} src={Columno} alt="logo" />{" "}
-          {/* <span className="block text-xl">olumno</span> */}
         </li>
-        <span className="flex">
+        <li className="flex">
           <div className="flex rounded-lg">
             {/* <NavbarItem href="/demo" className="rounded-l-lg">Try the app</NavbarItem> */}
             <NavbarItem href="/contact" className="rounded-r-lg">
-              Contact Developer
+              Contact <span className="max-sm:hidden">Développeur</span>
             </NavbarItem>
           </div>
-          <TryItOut className="ml-3" />
-        </span>
+          <TryItOut className="hidden md:flex" />
+        </li>
       </ul>
     </nav>
   );
--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -6,7 +6,7 @@ import AppPresentation from "./components/app-presentation";
 function LandingPage() {
   return (
     <Container as={"div"} className="bg-black bg-fixed">
-      <Container className="px-12 pt-6 text-white min-h-screen flex flex-col 2xl:px-40 3xl:px-60 4xl:w-3/4 4xl:mx-auto 2xl:pt-6">
+      <Container className="px-6 pt-6 md:px-12 text-white min-h-screen flex flex-col 2xl:px-40 3xl:px-60 4xl:w-3/4 4xl:mx-auto 2xl:pt-6">
         <Container as={"header"}>
           <Navbar />
         </Container>
