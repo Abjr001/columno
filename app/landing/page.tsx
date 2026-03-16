fef7cc3ab0117f04bb481bc60fa7b67a7b3b820c
Kekeli <keliabotsidia@gmail.com>
2026-03-16 05:26:43 +0000

    responsive mobile updated

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -6,11 +6,11 @@ import AppPresentation from "./components/app-presentation";
 function LandingPage() {
   return (
     <Container as={"div"} className="bg-black bg-fixed">
-      <Container className="px-6 pt-6 md:px-12 text-white min-h-screen flex flex-col 2xl:px-40 3xl:px-60 4xl:w-3/4 4xl:mx-auto 2xl:pt-6">
+      <Container className="px-2 pt-6 md:px-12 text-white min-h-screen flex flex-col 2xl:px-40 3xl:px-60 4xl:w-3/4 4xl:mx-auto 2xl:pt-6">
         <Container as={"header"}>
           <Navbar />
         </Container>
-        <Container className="flex-1 flex items-center" as={"main"}>
+        <Container className="md:flex-1 flex items-center" as={"main"}>
           <Container as={"section"} className="w-full">
             <AppPresentation />
           </Container>
